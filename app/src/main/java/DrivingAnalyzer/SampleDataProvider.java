740ef36c261842fdcf3e38d676fb614e987cec72
Marek Fudaliński <fudalinm@gmail.com>
2020-11-22 22:20:46 +0100

    POC of detecting anomaly; interface description; and writing basic anomaly detection that will be changed in the future

---
+++ app/src/main/java/DrivingAnalyzer/SampleDataProvider.java
@@ -0,0 +1,8 @@
+package DrivingAnalyzer;
+
+public class SampleDataProvider implements DataProvider {
+    @Override
+    public void getData() {
+
+    }
+}
---
+++ app/src/main/java/DrivingAnalyzer/SampleDrivingAnalyzer.java
@@ -0,0 +1,4 @@
+package DrivingAnalyzer;
+
+public class SampleDrivingAnalyzer {
+}
